55202bd55362fc1fcbff03dd9d7af66e58a2b9d6
patito <p.sotosellanes@hotmail.com>
2020-11-20 19:53:13 -0300

    added search function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,23 @@
 class ProductsController < ApplicationController
 
   def index
-    @products = Product.all
+    
   end
 
-  def categories
+  def all_categories
     @categories = Category.all
 
     render json: @categories
   end    
+
+  def all_products
+    products = nil
+    if (params[:category])
+      products = Product.where("category = #{(params[:category])}")
+    else (params[:name])  
+      products = Product.where('name LIKE ?', "%#{params[:name]}%")
+    end  
+    render json: products.to_json
+  end  
+
 end
