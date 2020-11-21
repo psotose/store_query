712524947d138ebea1ffcf8c99a0a6c94f3171cb
patito <p.sotosellanes@hotmail.com>
2020-11-21 12:22:53 -0300

    card img

--- app/assets/javascripts/front.js
+++ app/assets/javascripts/front.js
@@ -1,4 +1,4 @@
-function getData() {
+function loadData() {
   fetch("/products/all_categories")
     .then((response) => response.json())
     .then((categories) => {
@@ -10,8 +10,13 @@ function getData() {
       });
     });
 
-  alert("sdsdsdsd");
   getProducts("", "");
+}
+
+function clickFind() {
+  var input = document.getElementById("name_product");
+  var select = document.getElementById("categories_select");
+  getProducts(input.value, select.value);
 }
 
 function getProducts(category, name) {
@@ -19,12 +24,18 @@ function getProducts(category, name) {
     .then((response) => response.json())
     .then((products) => {
       var div = document.getElementById("list_products");
-
+      var imgNot = document.getElementById("img_not_found");
+      var imgAdd = document.getElementById("addcart");
       products.forEach((element) => {
+        var image = imgNot.src;
+        if (element.url_image && element.url_image != "") {
+          image = element.url_image;
+        }
+
         var product = `<div class="col mb-2">
         <div class="card h-100 border border-success">
           <div class="card-img-top">
-            <img src="" class:"card-img-top mt-2" width="300" height="300"/>
+            <img src="${image}" class:"card-img-top mt-2" width="330" height="300"/>
           </div>
           <div class="card-body">
             <h5 class="card-title">${element.name}</h5>
@@ -35,7 +46,7 @@ function getProducts(category, name) {
                 <p class="text-success font-weight-bold pt-3 pb-0 pl-4">${element.price}</p>
               </div>
               <div class="col-4">
-                <p class="pt-2"><img src="" alt="Add Product to Cart" style="height: 2em" /></p>
+                <p class="pt-2"><img src="${imgAdd.src}" alt="Add Product to Cart" style="height: 2em" /></p>
               </div>
             </div>
           </div>
